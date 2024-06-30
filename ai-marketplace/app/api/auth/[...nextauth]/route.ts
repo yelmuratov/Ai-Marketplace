eb2ed3cbca8b19311b9f6fb1ed482becacab14c9
Salimbay <elmuratovsal1mbay@gmail.com>
2024-06-30 22:25:50 +0500

    commit

--- ai-marketplace/app/api/auth/[...nextauth]/route.ts
+++ ai-marketplace/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,7 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import { authOptions } from "@/lib/authOption";
+import NextAuth from "next-auth/next";
 
-export const authOptions: AuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    // Add more providers here
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export {handler as GET,handler as POST};
