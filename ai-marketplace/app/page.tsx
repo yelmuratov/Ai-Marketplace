1ccec9bec796ba29ddeb0ff8a95500c2409790bf
yelmuratov <elmuratovsal1mbay@gmail.com>
2024-10-05 12:43:38 +0500

    odal removed

--- ai-marketplace/app/page.tsx
+++ ai-marketplace/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import Head from "next/head";
-import ThreeScene from "./components/Threescene";
-import RegisterModal from "./modals/register-modal";
-import { useRegisterModal } from "@/stores/auth-store";
-import LoginModal from "./modals/login-modal";
+import ThreeScene from "./components/Threescene";;
 import Link from "next/link"; // Import Link component
 import { Toaster } from "react-hot-toast";
 
 const Home: React.FC = () => {
-  const { isRegisterModalOpen } = useRegisterModal();
 
   return (
     <div className="relative h-screen w-screen overflow-hidden">
@@ -37,7 +33,7 @@ const Home: React.FC = () => {
         <meta property="og:type" content="website" />
       </Head>
       <ThreeScene />
-      <div className={`absolute inset-0 flex flex-col items-center justify-center text-white z-10 p-4 ${isRegisterModalOpen ? 'backdrop-blur-sm' : ''} pointer-events-none`}>
+      <div className={`absolute inset-0 flex flex-col items-center justify-center text-white z-10 p-4 pointer-events-none`}>
         <h1 className="text-3xl lg:w-[1000px] text-center sm:text-4xl md:text-5xl lg:text-6xl mb-2 sm:mb-4">
           A marketplace for small businesses to easily use AI tools
         </h1>
@@ -49,8 +45,6 @@ const Home: React.FC = () => {
       <Link href="/joinlist" className="absolute z-40 top-1/2 mt-[140px] transform -translate-y-1/2 left-1/2 transform -translate-x-1/2 mt-4 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-md transition duration-300">
         Get Started
       </Link>
-      <RegisterModal />
-      <LoginModal />
       <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
